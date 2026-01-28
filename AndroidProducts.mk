d6975a30da7da96c6b17fc9b1ef825984dd22ad2
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-01-28 22:03:52 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_capri.mk
+    $(LOCAL_DIR)/infinity_capri.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_capri-user \
+    infinity_capri-userdebug \
+    infinity_capri-eng
