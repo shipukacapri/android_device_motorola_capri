d288142124956541ae7163c099c7f380bbf9c1ad
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-19 14:22:48 +0530

    capri: axionOS BringUp

--- lineage_capri.mk
+++ lineage_capri.mk
@@ -13,6 +13,13 @@ $(call inherit-product, device/motorola/capri/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#Axion Stuff
+TARGET_ENABLE_BLUR := true
+AXION_CAMERA_REAR_INFO := 48,8,2,2
+AXION_CAMERA_FRONT_INFO := 8
+AXION_MAINTAINER := Shipu
+AXION_PROCESSOR := SM4250
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_capri
 PRODUCT_DEVICE := capri
