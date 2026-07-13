637a1c6c7247f05ee994a3ae64d6d5175d999619
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-07-13 07:29:56 +0530

    make vanilla

--- lineage_capri.mk
+++ lineage_capri.mk
@@ -13,6 +13,8 @@ $(call inherit-product, device/motorola/capri/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+WITH_GMS := false
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_capri
 PRODUCT_DEVICE := capri
