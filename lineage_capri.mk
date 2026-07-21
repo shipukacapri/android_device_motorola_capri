aa64db45f49a0cfd4c80170940c00b6f2d3160ac
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-07-21 05:30:55 +0530

    make gapps

--- lineage_capri.mk
+++ lineage_capri.mk
@@ -13,7 +13,7 @@ $(call inherit-product, device/motorola/capri/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-WITH_GMS := false
+WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_capri
