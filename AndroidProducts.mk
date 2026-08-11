7247a14ed74336631b20c494baffe785caeabbff
shipukacapri <242039583+shipukacapri@users.noreply.github.com>
2026-08-11 20:36:07 +0530

    capri: InfinityX Bringup, Add Motocam

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_capri.mk
+    $(LOCAL_DIR)/infinity_capri.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_capri-user \
+    infinity_capri-userdebug \
+    infinity_capri-eng
--- infinity_capri.mk
+++ infinity_capri.mk
@@ -10,11 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/capri/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+# Infinity Flags
+INFINITY_MAINTAINER := Shipu
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_capri
+PRODUCT_NAME := infinity_capri
 PRODUCT_DEVICE := capri
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(10)
