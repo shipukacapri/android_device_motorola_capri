891ec4dfc60d1381d50ead8d9ce82825b0290152
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-08-31 10:54:41 +0530

    capri: rising bringup

--- lineage_capri.mk
+++ lineage_capri.mk
@@ -26,3 +26,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="capri_retail-user 11 RRB31.Q1-3-48-24 196a2 release-keys" \
     BuildFingerprint=motorola/capri_retail/capri:11/RRB31.Q1-3-48-24/196a2:user/release-keys \
     DeviceProduct=capri_retail
+
+# Rising Flags
+WITH_GMS := true
+RISING_MAINTAINER := Shipu
+TARGET_ENABLE_BLUR := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingMaintainer="Shipu" \
+    RisingChipset="Snapdragon 460"
