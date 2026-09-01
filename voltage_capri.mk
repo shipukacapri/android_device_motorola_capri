6af3e38189b7ee69fd6bcb3b34f8bd271d252fb8
shipukacapri <242039583+shipukacapri@users.noreply.github.com>
2026-09-01 11:41:08 +0530

    capri: voltageOS bringup

--- voltage_capri.mk
+++ voltage_capri.mk
@@ -10,13 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/capri/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-WITH_GMS := true
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_capri
+PRODUCT_NAME := voltage_capri
 PRODUCT_DEVICE := capri
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(10)
